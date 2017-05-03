548dbc05a567abffe1b6125d4a1db40e38d48898
jguardado39 <jguardado39@gmail.com>
2017-05-02 20:29:42 -0700

    done

---
+++ 2_Javascript_Where_To/2_JavaScript_Where_To.js
@@ -0,0 +1,9 @@
+// External scripots cannot contain <scrpt> tags
+// External JavaSript Advantages
+// It separates HTML and code
+// It makes HTML and JavaScript easier to read and maintain
+// Cached JavaScript file can speed up page loads
+
+function myFunction_3() {
+	document.getElementById("demo_4").innerHTML = "Paragraph changed.";
+}
